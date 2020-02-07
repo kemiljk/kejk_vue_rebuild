118678417b8c592daa8f96027407d6c0cfa74fe9
Karl Koch <hi@kejk.co>
2020-02-07 15:26:04 +0000

    Added in new links for email and music

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,22 @@
         />
       </div>
       <v-spacer></v-spacer>
+      <v-btn href="mailto:hi@kejk.co" target="_blank" text light>
+        <span class="mr-2">Email</span>
+      </v-btn>
       <v-btn href="https://www.twitter.com/_kejk" target="_blank" text light>
         <span class="mr-2">Twitter</span>
       </v-btn>
       <v-btn href="https://github.com/kemiljk" target="_blank" text light>
         <span class="mr-2">Github</span>
+      </v-btn>
+      <v-btn
+        href="https://music.apple.com/gb/album/cornerstone/1300802348"
+        target="_blank"
+        text
+        light
+      >
+        <span class="mr-2">Music</span>
       </v-btn>
     </v-app-bar>
 
