fcc6c4559e1aed3ef83014014f7fa69350035c22
Karl Koch <hi@kejk.co>
2020-02-08 11:16:04 +0000

    Changed preformatted code to be download buttons

--- src/views/Uses.vue
+++ src/views/Uses.vue
@@ -57,121 +57,21 @@
             >JetBrains Mono</a> as my default code font with ligatures set to true.
           </li>
         </ul>
-        <pre>
-          <v-application code>
-            {
-            <span class="json-attribute">"files.autoSave"</span>: "onFocusChange",
-            <span class="json-attribute">"editor.fontFamily"</span>: "JetBrains Mono",
-            <span class="json-attribute">"editor.fontLigatures"</span>: <span class="json-boolean">true</span>,
-            <span class="json-attribute">"editor.formatOnPaste"</span>: <span class="json-boolean">true</span>,
-            <span class="json-attribute">"editor.formatOnSave"</span>: <span class="json-boolean">true</span>,
-            <span class="json-attribute">"auto-close-tag.SublimeText3Mode"</span>: <span class="json-boolean">true</span>,
-            <span class="json-attribute">"workbench.colorCustomizations"</span>: {
-                "editor.background": "#050505",
-                "sideBar.background": "#111",
-                "terminal.background": "#050505",
-                "panel.background": "#050505",
-                "activityBar.background": "#050505",
-                "tab.inactiveBackground": "#1d1d1d",
-            },
-            <span
-  class="json-attribute"
->"workbench.colorTheme"</span>: "Seti Black",
-            <span class="json-attribute">"workbench.iconTheme"</span>: "material-icon-theme",
-            <span class="json-attribute">"vsicons.dontShowNewVersionMessage"</span>: <span class="json-boolean">true</span>,
-            <span class="json-attribute">"prettier.tabWidth"</span>: <span class="json-boolean">4</span>,
-            <span class="json-attribute">"prettier.useTabs"</span>: <span class="json-boolean">true</span>,
-            <span class="json-attribute">"editor.fontSize"</span>: <span class="json-boolean">11</span>,
-            <span class="json-attribute">"workbench.editor.showTabs"</span>: <span class="json-boolean">true</span>,
-            <span class="json-attribute">"editor.minimap.enabled"</span>: <span class="json-boolean">false</span>,
-            <span class="json-attribute">"editor.codeLens"</span>: <span class="json-boolean">false</span>,
-            <span class="json-attribute">"workbench.activityBar.visible"</span>: <span class="json-boolean">false</span>,
-            <span class="json-attribute">"explorer.openEditors.visible"</span>: <span class="json-boolean">0</span>,
-            <span class="json-attribute">"workbench.sideBar.location"</span>: "right",
-            <span class="json-attribute">"workbench.fontAliasing"</span>: "antialiased",
-            <span class="json-attribute">"editor.lineHeight"</span>: <span class="json-boolean">24</span>,
-            <span class="json-attribute">"editor.letterSpacing"</span>: <span class="json-boolean">0</span>,
-            <span class="json-attribute">"editor.emptySelectionClipboard"</span>: <span class="json-boolean">false</span>,
-            <span class="json-attribute">"editor.quickSuggestionsDelay"</span>: <span class="json-boolean">0</span>,
-            <span class="json-attribute">"editor.quickSuggestions"</span>: {
-                <span class="json-attribute">"strings"</span>: <span class="json-boolean">true</span>,
-            },
-            <span class="json-attribute">"editor.formatOnType"</span>: <span class="json-boolean">true</span>,
-            <span class="json-attribute">"vetur.grammar.customBlocks"</span>: {
-                <span class="json-attribute">"docs"</span>: "md",
-                <span class="json-attribute">"i18n"</span>: "json",
-                <span class="json-attribute">"preview-props"</span>: "json"
-            },
-            <span class="json-attribute">"workbench.startupEditor"</span>: "newUntitledFile",
-            <span class="json-attribute">"[vue]"</span> {
-                <span class="json-attribute">"editor.defaultFormatter"</span>: "octref.vetur"
-            },
-            <span class="json-attribute">"editor.tabSize"</span>: <span class="json-boolean">4</span>,
-            }
-          </v-application>
-        </pre>
+        <v-btn
+          href="https://res.cloudinary.com/kejk/raw/upload/v1581160468/settings.json_engjvv.zip"
+          target="_blank"
+          class="mb-3"
+        >Download JSON File</v-btn>
         <ul class="body-1 mb-3">
           <li>
             <a href="https://hyper.is" target="_blank">Hyper</a> is my terminal of choice with JetBrains Mono set as my font of choice as well as some custom colour rules.
           </li>
         </ul>
-        <pre>
-          <v-application code>
-            <span class="js-attribute">module.</span><span class="js-function">exports</span><span class="js-attribute"> =</span> {
-              <span class="teal--text">config</span><span class="js-value">:</span> {
-              <span class="teal--text">updateChannel</span><span class="js-value">:</span> <span class="teal--text">"stable"</span>,
-              <span class="teal--text">fontSize</span><span class="js-value">:</span> <span class="js-value">12</span>,
-              <span class="teal--text">fontFamily</span><span class="js-value">:</span> <span class="teal--text">"JetBrains Mono"</span>,
-              <span class="teal--text">fontWeight</span><span class="js-value">:</span> <span class="teal--text">"normal"</span>,
-              <span class="teal--text">fontWeightBold</span><span class="js-value">:</span> <span class="teal--text">"bold"</span>,
-              <span class="teal--text">lineHeight</span><span class="js-value">:</span> <span class="js-value">1.5</span>,
-              <span class="teal--text">letterSpacing</span><span class="js-value">:</span> <span class="js-value">0</span>,
-              <span class="teal--text">cursorColor</span><span class="js-value">:</span> <span class="teal--text">"#FFDD05"</span>,
-              <span class="teal--text">cursorAccentColor</span><span class="js-value">:</span> <span class="teal--text">"#191919"</span>,
-              <span class="teal--text">cursorShape</span><span class="js-value">:</span> <span class="teal--text">"BLOCK"</span>,
-              <span class="teal--text">cursorBlink</span><span class="js-value">:</span> <span class="js-value">false</span>,
-              <span class="teal--text">foregroundColor</span><span class="js-value">:</span> <span class="teal--text">"#f6f7f8"</span>,
-              <span class="teal--text">backgroundColor</span><span class="js-value">:</span> <span class="teal--text">"#191919"</span>,
-              <span class="teal--text">selectionColor</span><span class="js-value">:</span> <span class="teal--text">"rgba(248,28,229,0.3)"</span>,
-              <span class="teal--text">borderColor</span><span class="js-value">:</span> <span class="teal--text">"#333"</span>,
-              <span class="teal--text">css</span><span class="js-value">:</span> <span class="teal--text">""</span>,
-              <span class="teal--text">termCSS</span><span class="js-value">:</span> <span class="teal--text">""</span>,
-              <span class="teal--text">showHamburgerMenu</span><span class="js-value">:</span> <span class="teal--text">""</span>,
-              <span class="teal--text">showWindowControls</span><span class="js-value">:</span> <span class="teal--text">""</span>,
-              <span class="teal--text">padding</span><span class="js-value">:</span> <span class="teal--text">"12px 14px"</span>,
-              <span class="teal--text">colors</span><span class="js-value">:</span> {
-                  <span class="teal--text">black</span><span class="js-value">:</span> <span class="teal--text">"#191919"</span>,
-                  <span class="teal--text">red</span><span class="js-value">:</span> <span class="teal--text">"#C51E14"</span>,
-                  <span class="teal--text">green</span><span class="js-value">:</span> <span class="teal--text">"#1DC121"</span>,
-                  <span class="teal--text">yellow</span><span class="js-value">:</span> <span class="teal--text">"#FFDD05"</span>,
-                  <span class="teal--text">blue</span><span class="js-value">:</span> <span class="teal--text">"#1c475c"</span>,
-                  <span class="teal--text">magenta</span><span class="js-value">:</span> <span class="teal--text">"#C839C5"</span>,
-                  <span class="teal--text">cyan</span><span class="js-value">:</span> <span class="teal--text">"#20C5C6"</span>,
-                  <span class="teal--text">white</span><span class="js-value">:</span> <span class="teal--text">"#C7C7C7"</span>,
-                  <span class="teal--text">lightBlack</span><span class="js-value">:</span> <span class="teal--text">"#686868"</span>,
-                  <span class="teal--text">lightRed</span><span class="js-value">:</span> <span class="teal--text">"#FD6F6B"</span>,
-                  <span class="teal--text">lightGreen</span><span class="js-value">:</span> <span class="teal--text">"#67F86F"</span>,
-                  <span class="teal--text">lightYellow</span><span class="js-value">:</span> <span class="teal--text">"#FFFA72"</span>,
-                  <span class="teal--text">lightBlue</span><span class="js-value">:</span> <span class="teal--text">"#56bdf2"</span>,
-                  <span class="teal--text">lightMagenta</span><span class="js-value">:</span> <span class="teal--text">"#FD7CFC"</span>,
-                  <span class="teal--text">lightCyan</span><span class="js-value">:</span> <span class="teal--text">"#68FDFE"</span>,
-                  <span class="teal--text">lightWhite</span><span class="js-value">:</span> <span class="teal--text">"#FFFFFF"</span>
-                },
-                <span class="teal--text">shell</span><span class="js-value">:</span> <span class="teal--text">""</span>,
-                <span class="teal--text">shellArgs</span><span class="js-value">:</span> [<span class="teal--text">"--login"</span>],
-                <span class="teal--text">env</span><span class="js-value">:</span> {},
-                    <span class="teal--text">bell</span><span class="js-value">:</span> <span class="teal--text">"SOUND"</span>,
-                    <span class="teal--text">copyOnSelect</span><span class="js-value">:</span> <span class="js-value">false</span>,
-                    <span class="teal--text">defaultSSHApp</span><span class="js-value">:</span> <span class="js-value">true</span>,
-                    <span class="teal--text">quickEdit</span><span class="js-value">:</span> <span class="js-value">false</span>,
-                    <span class="teal--text">macOptionSelectionMode</span><span class="js-value">:</span> <span class="teal--text">"vertical"</span>,
-                <span class="teal--text">webGLRenderer</span><span class="js-value">:</span> <span class="js-value">true</span>
-                },
-              <span class="teal--text">plugins</span><span class="js-value">:</span> [<span class="teal--text">"hyper-fading-scrollbar"</span>],
-              <span class="teal--text">localPlugins</span><span class="js-value">:</span> [],
-            };
-          </v-application>
-        </pre>
+        <v-btn
+          href="https://res.cloudinary.com/kejk/raw/upload/v1581160309/hyper.js_wyc9e6.zip"
+          target="_blank"
+          class="mb-3"
+        >Download JS File</v-btn>
         <ul class="body-1 mb-3">
           <li>
             <a href="https://ohmyz.sh" target="_blank">ZSH</a> is my default terminal shell
