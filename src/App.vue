064c1306ed2fbd956e6d22833822c9b81fabe63e
Karl Koch <hi@kejk.co>
2020-02-10 16:34:05 +0000

    Fixing broken FAB footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app id="app" :dark="$isDark">
+  <v-app :dark="$isDark">
     <v-app-bar app>
       <v-toolbar-title class="logo">KEJK</v-toolbar-title>
       <v-spacer></v-spacer>
@@ -19,65 +19,52 @@
         <router-view></router-view>
       </v-responsive>
     </v-content>
-    <v-footer app>
-      <v-row justify="end" no-gutters>
-        <!-- <v-btn href="https://www.twitter.com/_kejk" target="_blank" text>Twitter</v-btn>
-        <v-btn href="https://github.com/kemiljk" target="_blank" text>Github</v-btn>
-        <v-btn
-          href="https://music.apple.com/gb/album/cornerstone/1300802348"
-          target="_blank"
-          text
-        >Music</v-btn> -->
-      <v-speed-dial
-      v-model="fab"
-      :top="top"
-      :bottom="bottom"
-      :right="right"
-      :left="left"
-      :direction="direction"
-      :transition="transition"
+    <v-footer app
     >
-      <template v-slot:activator>
-        <v-btn
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+      <v-row justify="end" no-gutters>
+        <v-speed-dial
           v-model="fab"
-          color="orange"
-          fab
+          absolute
+          bottom
+          right
         >
-          <v-icon v-if="fab">mdi-close</v-icon>
-          <v-icon v-else>mdi-account-circle</v-icon>
-        </v-btn>
-      </template>
-      <v-btn
-        fab
-        dark
-        small
-        color="teal"
-        href="mailto:hi@kejk.co"
-      >
-        <v-icon>mdi-email</v-icon>
-      </v-btn>
-      <v-btn
-        fab
-        dark
-        small
-        color="light-blue"
-        href="https://www.twitter.com/_kejk"
-        target="_blank"
-      >
-        <v-icon>mdi-twitter</v-icon>
-      </v-btn>
-      <v-btn
-        fab
-        dark
-        small
-        color="red"
-        href="https://music.apple.com/gb/album/cornerstone/1300802348"
-        target="_blank"
-      >
-        <v-icon>mdi-music</v-icon>
-      </v-btn>
-    </v-speed-dial>
+          <template v-slot:activator>
+            <v-btn v-model="fab" color="orange" fab>
+              <v-icon v-if="fab">mdi-close</v-icon>
+              <v-icon v-else>mdi-account-circle</v-icon>
+            </v-btn>
+          </template>
+          <v-btn fab dark small color="teal" href="mailto:hi@kejk.co">
+            <v-icon>mdi-email</v-icon>
+          </v-btn>
+          <v-btn
+            fab
+            dark
+            small
+            color="light-blue"
+            href="https://www.twitter.com/_kejk"
+            target="_blank"
+          >
+            <v-icon>mdi-twitter</v-icon>
+          </v-btn>
+          <v-btn
+            fab
+            dark
+            small
+            color="red"
+            href="https://music.apple.com/gb/album/cornerstone/1300802348"
+            target="_blank"
+          >
+            <v-icon>mdi-music</v-icon>
+          </v-btn>
+        </v-speed-dial>
       </v-row>
+        {{ new Date().getFullYear() }} — <strong>© Karl Koch</strong>
+      </v-col>
     </v-footer>
   </v-app>
 </template>
