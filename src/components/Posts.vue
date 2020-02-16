d3206f0f5816905e85c3586025291c3496ee82da
Karl Koch <hi@kejk.co>
2020-02-16 21:17:45 +0000

    Some fixes

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,7 +1,11 @@
 <template>
   <v-card hover :href="`${post.url}`" :target="`${post.ext}`">
     <v-img :src="`${post.img}`"></v-img>
-    <v-card-title class="body-2">{{ post.title }}</v-card-title>
+    <v-card-title class="body-2">
+      {{ post.title }}
+      <v-spacer />
+      <v-icon class="ml-4">mdi-arrow-right</v-icon>
+    </v-card-title>
   </v-card>
 </template>
 
