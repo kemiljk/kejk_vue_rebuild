c373ab9b99b978b1a28551341cddcce6f2592b4b
Karl <kemiljk@users.noreply.github.com>
2020-03-16 22:06:34 +0000

    fix: whoops, missing comma... again!

--- src/router/index.js
+++ src/router/index.js
@@ -84,7 +84,7 @@ const routes = [
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
   scrollBehavior() {
         document.getElementById('app').scrollIntoView();
     }
