686ee43d4b8bf2e1600951af32041604c4f22e9e
Karl <kemiljk@users.noreply.github.com>
2020-03-26 20:59:04 +0000

    fixes: whoops, missing comma

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
   lintOnSave: true,
   filenameHashing: false,
   chainWebpack: config => {
-    config.optimization.delete('splitChunks')
+    config.optimization.delete('splitChunks'),
 
   runtimeCompiler: true,
   productionSourceMap: false,
