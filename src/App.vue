5d67b0b7c3bac009936193aa23e34625d738accf
Karl Koch <hi@kejk.co>
2020-04-06 18:30:31 +0100

    feat: initial skeleton commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
-  <v-app :dark="$isDark">
+  <v-app>
     <v-app-bar app>
-      <v-toolbar-title class="logo d-none d-sm-block font-weight-black">KEJK</v-toolbar-title>
-      <v-toolbar-title class="logo d-sm-none font-weight-black">K</v-toolbar-title>
+        <v-img 
+          src="./assets/Lazy-PDF-icon.png"
+          max-height="40"
+          max-width="40"
+          contain
+        />
       <v-spacer />
-      <v-btn icon class="mr-1" alt="Toggle colour theme" @click="toggleTheme">
-        <v-icon v-if="this.$vuetify.theme.dark">mdi-white-balance-sunny</v-icon>
-        <v-icon v-else>mdi-brightness-3</v-icon>
+      <v-btn 
+        icon 
+        class="mr-1" 
+        alt="Toggle colour theme" 
+        @click="toggleTheme">
+        <v-icon v-if="this.$vuetify.theme.dark">
+          mdi-white-balance-sunny
+        </v-icon>
+        <v-icon v-else>
+          mdi-brightness-3
+        </v-icon>
       </v-btn>
       <v-btn
         v-for="link in links"
@@ -15,8 +27,19 @@
         text
         :to="link.url"
       >
-        <v-text class="font-weight-bold">{{ link.label }}</v-text>
+        <v-text class="font-weight-bold">
+          {{ link.label }}
+        </v-text>
       </v-btn>
+     <v-btn
+        class="my-2 mx-1"
+        text
+        href="https://res.cloudinary.com/kejk/raw/upload/v1585911201/Lazy_PDF.zip"
+      >
+        <v-text class="font-weight-bold">
+          Download
+        </v-text>
+      </v-btn> 
     </v-app-bar>
     <v-content>
       <v-responsive width="900px">
@@ -24,75 +47,7 @@
       </v-responsive>
     </v-content>
     <v-footer app>
-      <v-col class="text-left" cols="12">
-        <v-row justify="end" no-gutters>
-          <v-speed-dial v-model="fab" absolute bottom right alt="Contact Karl">
-            <template v-slot:activator>
-              <v-btn v-model="fab" color="teal" fab>
-                <v-icon v-if="fab" class="white--text">mdi-close</v-icon>
-                <v-icon v-else class="white--text">mdi-account-circle</v-icon>
-              </v-btn>
-            </template>
-            <v-btn fab dark small color="teal" href="mailto:karl@kejk.tech" alt="Email Karl">
-              <v-icon>mdi-email</v-icon>
-            </v-btn>
-            <v-btn
-              fab
-              dark
-              small
-              color="light-blue"
-              href="https://www.twitter.com/_kejk"
-              target="_blank"
-              alt="Karl's Twitter"
-            >
-              <v-icon>mdi-twitter</v-icon>
-            </v-btn>
-            <v-btn
-              fab
-              light
-              small
-              color="white"
-              href="https://www.github.com/kemiljk/"
-              target="_blank"
-              alt="Karl's GitHub"
-            >
-              <v-icon>mdi-git</v-icon>
-            </v-btn>
-            <v-btn
-              fab
-              dark
-              small
-              color="blue"
-              href="https://www.linkedin.com/in/kejk"
-              target="_blank"
-              alt="Karl's LinkedIn"
-            >
-              <v-icon>mdi-linkedin</v-icon>
-            </v-btn>
-            <v-btn
-              fab
-              dark
-              small
-              color="#6670f9"
-              href="https://dev.to/_kejk"
-              target="_blank"
-              alt="Karl's LinkedIn"
-            >
-              <v-icon>mdi-dev-to</v-icon>
-            </v-btn>
-            <v-btn
-              fab
-              dark
-              small
-              color="red"
-              href="https://music.apple.com/gb/album/cornerstone/1300802348"
-              target="_blank"
-              alt="Karl's Music with Echoes"
-            >
-              <v-icon>mdi-music</v-icon>
-            </v-btn>
-          </v-speed-dial>
-        </v-row>
+      <v-col cols="12">
         <strong>
           Made with
           <v-icon>mdi-vuejs</v-icon>in London.
@@ -111,14 +66,6 @@ export default {
         {
           label: "About",
           url: "/"
-        },
-        {
-          label: "Lazy PDF",
-          url: "/lazy-pdf/"
-        },
-        {
-          label: "Login",
-          url: "/login"
         }
       ]
     };
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,334 +2,71 @@
   <v-container>
     <v-row class="text-left">
       <v-col>
-        <p class="text-uppercase title mt-10" style="line-height: 16pt">
-          Senior UX/UI Designer
-          <br />at
-          <strong>NeuerEnergy</strong>
-        </p>
-        <h1
-          class="display-3 font-weight-bold mb-10 pt-10"
-          aria-label="intro to Karl"
-        >Karl Emil James Koch.</h1>
-        <h2 class="display-1 font-weight-medium pt-10 mb-3">
-          I currently lead product design at
-          <a
-            href="https://www.neuerenergy.com"
-            class="teal--text font-weight-bold"
-            target="_blank"
-            rel="noreferrer"
-          >NeuerEnergy</a>
-          &nbsp;
-          <sup>
-            <v-icon class="teal--text" small>mdi-open-in-new</v-icon>
-          </sup> in London, UK.
-        </h2>
-      </v-col>
-      <v-col class="mb-5" cols="12">
-        <p
-          class="body-1 mb-3"
-          aria-label="about Karl"
-        >I started out my career with a deep love of psychology and a Masters in Fine Art creating behaviour-influencing sculptures and digital installations.</p>
-        <p
-          class="body-1 mb-3"
-        >Now, having spent 6 years bringing design thinking practices to rare diseases, I lead a product design team delivering elegant persona-driven solutions to change the face of renewable energy.</p>
-        <p
-          class="body-1 mb-3"
-        >I regularly practice mindfulness and meditation, and I’m an art, design and architecture nerd that absolutely loves exploring and learning about how anything is built.</p>
-        <p
-          class="body-1 mb-3"
-        >I closely follow Dieter Rams' Ten Principles Of Good Design, using these as a wayfinder when planning and executing on any design or development decision and any implementation detail. As true as they were in the 1970's, these declarations are even more relevant today in a world of complexity and abundance.</p>
-        <p class="body-1 mb-3">
-          Previously, I've worked for
-          <a
-            href="https://www.cognite.co"
-            target="_blank"
-            rel="noreferrer"
-            class="teal--text"
-          >Cognite</a>, Incuna (R.I.P.) and
-          <a
-            href="https://www.apple.com/retail/"
-            target="_blank"
-            rel="noreferrer"
-            class="teal--text"
-          >Apple</a>.
-        </p>
-      </v-col>
-    </v-row>
-    <v-divider />
-    <v-row class="text-left">
-      <v-col cols="6">
-        <h2
-          class="text-left display font-weight-bold mb-3 mt-10"
-          aria-label="Karl's portfolio"
-        >Portfolio.</h2>
-      </v-col>
-      <v-spacer />
-      <v-col cols="6" class="text-right mb-2">
-        <v-btn text class="mt-10 ml-4 mb-2" to="/login">
-          Login
-          <v-icon class="ml-2" small>mdi-lock</v-icon>
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-row class="text-left">
-      <v-col class="mb-3 py-0" cols="12" sm="6" md="4" lg="4">
-        <v-card href="mailto:karl@kejk.tech">
-          <v-img alt="Portfolio visual" src="../assets/portfolio.webp" />
-          <v-card-title class="subtitle-1">
-            Request access
-            <v-spacer />
-            <v-icon class="ml-4">mdi-email</v-icon>
-          </v-card-title>
-        </v-card>
-      </v-col>
-      <v-col cols="12" lg="8" md="8" sm="6">
-        <p
-          class="body-1"
-        >If you would like to see my portfolio or CV, you can request access by clicking the request access card. Due to the sensitivity of the projects I've worked on, I'm not permitted to list them here so I'll provide you with login access as required.</p>
-      </v-col>
-      <v-col class="mb-3 py-0" cols="12" sm="6" md="4" lg="4">
-        <v-card href="https://calendly.com/kejk/60min" target="_blank" rel="noreferrer">
-          <v-img alt="Calendar visual" src="../assets/calendly.webp" />
-          <v-card-title class="subtitle-1">
-            Book an appointment
-            <v-spacer />
-            <v-icon class="ml-4">mdi-calendar-clock</v-icon>
-          </v-card-title>
-        </v-card>
-      </v-col>
-      <v-col cols="12" lg="8" md="8" sm="6">
-        <p
-          class="body-1"
-        >If you want to book some time to have a chat, you can book a slot. I'll always make myself available for questions, advice or mentorship if I can.</p>
-        <p class="body-1">We could talk about:</p>
-        <ul>
-          <li>User experience, user interface, ally, design leadership</li>
-          <li>HTML, CSS/SCSS, Vue.js, Git</li>
-          <li>Minimalism, meditation, mindfulness</li>
-          <li>Guitars, metal or post rock</li>
-        </ul>
-      </v-col>
-    </v-row>
-    <v-row class="text-left">
-      <v-col cols="12">
-        <p class="body-1 mt-3 mb-3">
-          This website was designed and built by me in
-          <a
-            href="https://vuejs.org/"
-            target="_blank"
-            rel="noreferrer"
-            class="teal--text"
-          >Vue</a> utilising the
-          <a
-            href="https://vuetifyjs.com"
-            target="_blank"
-            rel="noreferrer"
-            class="teal--text"
-          >Vuetify</a> library. It's a good indicator of my personal design aesthetic and approach to development.
-        </p>
-      </v-col>
-    </v-row>
-    <v-divider />
-    <v-row class="text-left">
-      <v-col cols="12" aria-label="Karl's other interests">
-        <h2 class="display font-weight-bold mb-3 mt-6">On the side.</h2>
-      </v-col>
-      <v-col class="mb-3 py-0" cols="12" sm="6" md="4" lg="4">
-        <v-card href="https://www.makemeacocktail.com/" target="_blank" rel="noreferrer">
-          <v-img alt="Make Me A Cocktail visual" src="../assets/mmac.webp" />
-          <v-card-title class="subtitle-1">
-            Make Me A Cocktail
-            <v-spacer />
-            <v-icon class="ml-4">mdi-open-in-new</v-icon>
-          </v-card-title>
-        </v-card>
-      </v-col>
-      <v-col cols="12" lg="8" md="8" sm="6">
-        <h3 class="mb-3">Make Me A Cocktail</h3>
-        <p
-          class="body-1"
-        >I'm the Design Lead for Make Me A Cocktail. We're currently in the process of redesigning and rebuilding the site but many of my designs are now implemented across the core screens.</p>
-      </v-col>
-      <v-col class="mb-3 py-0" cols="12" sm="6" md="4" lg="4">
-        <v-card
-          href="https://res.cloudinary.com/kejk/raw/upload/v1585911201/Lazy_PDF.zip"
-          rel="noreferrer"
+        <h1 
+          class="display-2 font-weight-bold mb-3 mt-10" 
+          aria-label="Lazy PDF product"
         >
-          <v-img alt="Lazy PDF logo visual" src="../assets/lazyPDF.webp" />
-          <v-card-title class="subtitle-1">
-            Lazy PDF
-            <v-spacer />
-            <v-icon class="ml-4">mdi-download</v-icon>
-          </v-card-title>
-        </v-card>
+          Lazy-PDF
+        </h1>
+        <h2
+          class="display font-weight-bold mb-3 mt-10"
+          aria-label="Karl's posts"
+        >
+          Effortless and interfaceless PDF creation.
+        </h2>
+        <v-img
+          :src="`${hero}`"
+          class="mt-10 img"
+          alt="image of Lazy PDF app in use"
+        />
       </v-col>
-      <v-col cols="12" lg="8" md="8" sm="6">
+    </v-row>
+    <v-row class="text-left">
+        <v-col cols="12">
+          <a 
+          href="https://www.producthunt.com/posts/lazy-pdf?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-lazy-pdf" 
+          target="_blank" 
+        >
+          <img 
+            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=191366&theme=light" 
+            alt="Lazy PDF - Effortless and interfaceless PDF creation | Product Hunt Embed" 
+            style="width: 250px; height: 54px;" 
+            width="250px" 
+            height="54px" 
+          >
+        </a>
+      </v-col>
+      <v-col cols="12">
         <h3 class="mb-3">Lazy PDF</h3>
         <p
           class="body-1"
-        >
-          A free interfaceless macOS utility that allows you to effortlessly create a PDF from a set of selected images.
-        </p>
-        <v-btn
-          to="/lazy-pdf"
-          alt="link to url"
-        >
-          Lazy PDF
-          <v-icon 
-            class="ml-2"
-            small  
-          >
-            mdi-arrow-right
-          </v-icon>
-        </v-btn>
+        >Lazy PDF is a little free macOS utility that allows you to effortlessly create a PDF from a set of selected images. It's interfaceless and relies only on a Finder selection UI. 
+        
+        This is designed to be simple and have a low overhead to make it easier to grab a set of design files and throw them together into a PDF. 
+        
+        It'll listen to your logical file order, so number your files if you want to order in sequence, otherwise, you can move them around afterwards in Preview. 
+        
+        Lazy PDF respects your image sizes so doesn't try to compress everything to printable sizes, ideal for sharing work to teammates and management.</p>
       </v-col>
-      <v-col class="mb-3 py-0" cols="12" sm="6" md="4" lg="4">
-        <v-card href="https://www.kanic.co/" target="_blank" rel="noreferrer">
-          <v-img alt="Kanic logo visual" src="../assets/kanic.webp" />
-          <v-card-title class="subtitle-1">
-            Kanic
-            <v-spacer />
-            <v-icon class="ml-4">mdi-open-in-new</v-icon>
-          </v-card-title>
-        </v-card>
-      </v-col>
-      <v-col cols="12" lg="8" md="8" sm="6">
-        <h3 class="mb-3">Kanic</h3>
-        <p class="body-1">I'm also currently building something new with some great people.</p>
-      </v-col>
-    </v-row>
-    <v-divider />
-    <v-row class="ml-1 mr-1">
-      <v-col cols="6">
-        <h2
-          class="text-left display font-weight-bold mb-3 mt-10"
-          aria-label="Karl's posts"
-        >Latest Posts.</h2>
-      </v-col>
-      <v-spacer />
-      <v-col cols="6" class="text-right mb-2">
-        <v-btn text class="mt-10 ml-4 mb-2" to="/posts">
-          More
-          <v-icon class="ml-2">mdi-arrow-right</v-icon>
-        </v-btn>
-      </v-col>
-    </v-row>
-    <v-row class="mt-3 mb-5">
-      <v-col v-for="post in posts.slice(0, 2)" :key="`${post.id}`" cols="12" sm="6">
-        <Posts :post="post" class="justify-center" />
-      </v-col>
-    </v-row>
-    <v-divider />
-    <v-row class="text-left mt-10">
-      <v-col cols="6">
-        <h2 class="display font-weight-bold" aria-label="Karl's tech setup">Uses.</h2>
-      </v-col>
-      <v-spacer />
-      <v-col cols="6" class="text-right mb-2">
-        <v-btn text class="ml-4 mb-2" to="/uses">
-          My setup
-          <v-icon class="ml-2">mdi-arrow-right</v-icon>
-        </v-btn>
-      </v-col>
-      <v-col cols="12">
-        <p
-          class="body-1 mb-3"
-        >As a frontend designer, I use a variety of things to stay productive. Check out my hardware and software setup and get in touch if you have any questions.</p>
-      </v-col>
-    </v-row>
-    <v-divider />
-    <v-row class="ml-1 mr-1">
-      <h2 class="display font-weight-bold mb-3 mt-10" aria-label="Karl's posts">Currently reading.</h2>
-      <p class="body-1 mb-3 text-left">
-        I don't make any money from these links, but if you buy a book through one of the Amazon links then a portion of your purchase goes to charity thanks to
-        <a
-          href="https://smile.amazon.com"
-          target="_blank"
-          rel="noreferrer"
-          class="teal--text"
-        >Amazon Smile</a>.
-      </p>
-      <v-row>
-        <v-col v-for="book in books" :key="`${book.id}`" cols="6" md="3">
-          <Books :book="book" />
-        </v-col>
-      </v-row>
     </v-row>
     <v-row class="text-left">
-      <v-col class="mb-5">
-        <h2
-          class="display font-weight-bold mb-3"
-          aria-label="Karl's music playlist"
-        >Currently playing.</h2>
-        <v-row>
-          <v-col v-for="music in musics" :key="`${music.id}`" cols="12" sm="6">
-            <Musics :music="music" />
-          </v-col>
-        </v-row>
-      </v-col>
-    </v-row>
-    <v-divider />
-    <v-row class="text-left">
-      <v-col cols="12" class="mt-5 mb-5">
-        <p class="body-1">
-          This site was built in
-          <a
-            href="https://vuejs.org/"
-            target="_blank"
-            rel="noreferrer"
-            class="teal--text"
-          >Vue</a> utilising the
-          <a
-            href="https://vuetifyjs.com"
-            target="_blank"
-            rel="noreferrer"
-            class="teal--text"
-          >Vuetify</a> library and is deployed via
-          <a
-            href="https://zeit.co/"
-            target="_blank"
-            rel="noreferrer"
-            class="teal--text"
-          >Zeit Now</a>.
-        </p>
-        <p class="body-1">
-          The font rendered throughout is
-          <a
-            href="https://tribby.com/fonts/barlow/"
-            target="_blank"
-            rel="noreferrer"
-            class="teal--text"
-          >Barlow</a> by Jeremy Tribby. Many thanks to him for making it available open-source.
-        </p>
-        <p
-          class="body-1"
-        >This website doesn't use cookies or analytics scripts because I don't need to track you. All it does is use localstorage to save session state for faster re-loading.</p>
+      <v-col v-for="image in images" :key="`${image.id}`" cols="12">
+        <v-img :src="`${image.img}`" class="img" />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import Posts from "../components/Posts";
-import postsData from "../data/posts.json";
-import Books from "../components/Books";
-import booksData from "../data/books.json";
-import Musics from "../components/Musics";
-import musicsData from "../data/musics.json";
+import lazyPDF from "./../data/lazyPDF.json";
 
 export default {
-  name: "About",
-  components: {
-    Posts,
-    Books,
-    Musics
-  },
+  name: "LazyPDF",
   data() {
     return {
-      posts: postsData,
-      books: booksData,
-      musics: musicsData
+      images: lazyPDF,
+      hero: "https://res.cloudinary.com/kejk/image/upload/v1585992488/macbook-pro-space-gray-on-the-wooden-table_e6bu8w.jpg",
+      url: "https://res.cloudinary.com/kejk/raw/upload/v1585911201/Lazy_PDF.zip"
     };
   }
 };
