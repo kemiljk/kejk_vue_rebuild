f4e9613ae52f37dab4ca797951a7399d9f11aac1
Karl <kemiljk@users.noreply.github.com>
2020-07-13 08:34:05 +0100

    fix: typos

--- src/views/Uses.vue
+++ src/views/Uses.vue
@@ -74,7 +74,7 @@
               target="_blank"
               rel="noreferrer"
               class="teal--text"
-            >Figma</a> is my design tool for most projects. Its constantly updated and has such a flexible and vast ecosystem of plugins that I can't see myself using anything else. You can find me over at
+            >Figma</a> is my design tool of choice. It's constantly updated and has such a flexible and vast ecosystem of plugins that I can't see myself using anything else. You can find me over at
             <a
               href="https://www.figma.com/@_kejk"
               target="_blank"
