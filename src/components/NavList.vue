d8d6adbfbac147329d680f91cccfb3e2ef32307d
Karl Koch <hi@kejk.co>
2020-10-10 14:12:18 +0100

    feat: added in better nav and breakpoint classes to define what is shown when

--- src/components/NavList.vue
+++ src/components/NavList.vue
@@ -1,6 +1,6 @@
 <template>
-<v-list-item class="my-2 px-4 white--text" text :to="nav.url">
-    <v-list-item-content class="font-weight-medium white--text">{{
+<v-list-item class="my-2 px-4" text :to="nav.url">
+    <v-list-item-content class="font-weight-medium">{{
       nav.label
     }}</v-list-item-content>
 </v-list-item>
